a38c282c481fa6da33030b3a7a44b1073fb6a547
irsyad-muhamad <irsyad.doang78902gmail.com>
2021-09-19 17:51:31 +0700

    menyelesaikan tugas property dan method dengan file produk.php di video ke 3

--- produk.php
+++ produk.php
@@ -5,6 +5,11 @@ class produk {
            $penulis = "penulis",
            $penerbit = "penerbit",
            $harga = "0";
+
+           public function getLabel(){
+               return "$this->penulis, $this->penerbit";
+
+           }
 }
 
 // $produk1 = new produk();
@@ -22,4 +27,13 @@ $produk3->penulis = "Masashi Kishimoto";
 $produk3->penerbit = "Shounen Jump";
 $produk3->harga = "30000";
 
-echo "Komik : $produk3->penulis, $produk3->penerbit";
+
+$produk4 = new produk();
+$produk4->judul = "uncharted";
+$produk4->penulis = "Neil Druckman";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 25000;
+
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "game : " . $produk4->getLabel();
