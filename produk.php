64640076b6f5647b87c96338f12548d9e552e308
irsyad-muhamad <irsyad.doang78902gmail.com>
2021-09-19 16:29:17 +0700

    meneruskan tugas di video 2 yang belum beres

--- produk.php
+++ produk.php
@@ -1,11 +1,25 @@
 <?php
 
 class produk {
-    public $judul,
-           $penulis,
-           $penerbit,
-           $harga;
+    public $judul = "judul",
+           $penulis = "penulis",
+           $penerbit = "penerbit",
+           $harga = "0";
 }
 
-$produk1 = new produk();
-var_dump($produk1);
+// $produk1 = new produk();
+// $produk1->judul = "naruto";
+// var_dump($produk1);
+
+// $produk2 = new produk();
+// $produk2->judul = "uncharted";
+// $produk2->tambahproperty = "dibuat";
+// var_dump($produk2);
+
+$produk3 = new produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shounen Jump";
+$produk3->harga = "30000";
+
+echo "Komik : $produk3->penulis, $produk3->penerbit";
