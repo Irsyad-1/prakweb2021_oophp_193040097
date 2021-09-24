3234fac8ec380e9dc5d7598085608c57e7798b11
irsyad-muhamad <irsyad.doang78902gmail.com>
2021-09-24 11:24:13 +0700

    menyelesaikan tugas visibility.php di video visibility

---
+++ visibility.php
@@ -0,0 +1,94 @@
+<?php
+
+class Produk {
+    public $judul,
+           $penulis,
+           $penerbit;
+
+    protected $diskon = 0;
+
+    private $harga;
+
+           public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0) {
+               $this->judul = $judul;
+               $this->penulis = $penulis;
+               $this->penerbit = $penerbit;
+               $this->harga = $harga;
+           }
+
+
+           public function getharga() {
+            return $this->harga - ( $this->harga * $this->diskon / 100);
+        }
+    
+
+           public function getLabel(){
+               return "$this->penulis, $this->penerbit";
+
+           }
+           public function getinfoproduk() {
+               $str = "{$this->judul} | {$this->getlabel()} (Rp. {$this->harga})";
+
+               return $str;
+               
+           }
+}
+
+class Komik extends Produk {
+    public $jmlhalaman;
+
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", 
+    $harga = 0, $jmlhalaman = 0 ) {
+
+        parent::__construct( $judul, $penulis, $penerbit, $harga );
+
+        $this->jmlhalaman = $jmlhalaman;
+    }
+    
+    public function getinfoproduk() {
+        $str = "Komik : ". parent::getinfoproduk() ." - {$this->jmlhalaman} Halaman.";
+        return $str;
+    }
+}
+
+class game extends Produk {
+    public $waktumain;
+
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", 
+    $harga = 0, $waktumain = 0  ) {
+        parent::__construct( $judul, $penulis, $penerbit, $harga);
+
+        $this->waktumain = $waktumain;
+    }
+
+    public function setdiskon( $diskon) {
+        $this->diskon = $diskon;
+    }
+
+    
+    public function getinfoproduk() {
+        $str = "Game : ". parent::getinfoproduk() ." - {$this->waktumain} Jam.";
+        return $str;
+    }
+
+}
+
+class CetakInfoproduk {
+    public function cetak( Produk $produk) {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shounen Jump", 30000, 100,);
+$produk2 = new Game("uncharted", "Neil Druckman", "Sony Computer", 25000,50,);
+
+
+echo $produk1->getinfoproduk();
+echo "<br>";
+echo $produk2->getinfoproduk();
+echo "<hr>";
+
+$produk2->setdiskon(50);
+echo $produk2->getharga();
